e0576d2eabfef5c5c2dec74f82f163e1e1c3cc1f
ricardosullivan05-arch <ricardosullivan05@gmail.com>
2026-07-25 20:12:08 -0400

    Update BGS.lua

--- BGS.lua
+++ BGS.lua
@@ -1,4 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ricardosullivan05-arch/SoggyHub/main/BGS.lua"))()
 local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
 
 local win = DiscordLib:Window("Soggy Hub ")
